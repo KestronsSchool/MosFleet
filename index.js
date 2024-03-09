2f6224da49adcebaf1f69bd9de486d0077dbdbb0
KestronsSchool <132248969+KestronsSchool@users.noreply.github.com>
2024-03-08 18:51:55 -0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+const express=require("express");
+const fs=require("fs");
+const site=new express();
+site.listen(3000,()=>{
+    console.log("Propaganda site online");
+});
+site.use(express.static("./static"));
+site.use("/favicon.ico",express.static("./static/ferry.png"));
+site.get("/*",(req,res)=>{
+    res.send(fs.readFileSync(`./static/${req.originalUrl}.html`,"utf-8"));
+});
---
+++ static/header.js
@@ -0,0 +1,7 @@
+document.body.innerHTML=`
+<div class="banner">
+    <a href="/"><img src="ferry.png" id="icon" class="bannerImg"><img src="ferrybanner.png" id="banner" class="bannerImg"></a><br>
+    <a href="/" class="bannerBtn">Home</a><a href="/beginnings" class="bannerBtn">Beginnings</a><a href="/history" class="bannerBtn">Other History</a><a href="/routes" class="bannerBtn">Routes</a><a href="/safety" class="bannerBtn">Safety and Mishaps</a>
+</div>
+${document.body.innerHTML}
+<footer>Developed by Kestron for Professor Bill's CIS 260 UI Design Class Winter '24. Most all text taken from the <a href="https://en.wikipedia.org/wiki/Puget_Sound_mosquito_fleet">Wikipedia Article</a></footer>`;
